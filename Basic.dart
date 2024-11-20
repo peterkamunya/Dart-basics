caeef11d400a23530f7a03930db9919c93d6dbee
peterkamunya <113988753+peterkamunya@users.noreply.github.com>
2024-11-20 22:38:59 +0300

    Add files via upload

---
+++ Basic.dart
@@ -0,0 +1,17 @@
+  void main(){
+    var sam ="stephen";
+    sam ="pedroz";
+    print(sam);
+    const age =25;
+    print(age);
+    print(age+10);
+    print(age+100);
+    print(age/5);
+    print(age%5);
+
+    print("My name is "+ sam);
+    // you may use a concatination or a dollar sign to refering to name of variable.
+    print("my name is $sam and my age is ${age}");
+
+
+  }
---
+++ typeannotation.dart
@@ -0,0 +1,15 @@
+
+void main(){
+  // you  cant change the value to assinged type
+  String name ="pedroz";
+  print(name);
+  bool isopen =true;
+  print(isopen);
+  
+   double average =8.4;
+  print(average);
+
+
+
+
+}
